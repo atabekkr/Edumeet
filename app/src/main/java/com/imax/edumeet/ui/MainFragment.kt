2ffba8858ff242aea7f058085313ea673ad7542c
atabekkr <atabekdev@gmail.com>
2024-09-12 00:52:47 +0500

    added custom bottom bar

--- app/src/main/java/com/imax/edumeet/ui/MainFragment.kt
+++ app/src/main/java/com/imax/edumeet/ui/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.imax.edumeet.ui
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -8,6 +10,7 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.imax.edumeet.R
 import com.imax.edumeet.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainFragment : Fragment(R.layout.fragment_main) {
@@ -18,6 +21,21 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.cardReading.setOnClickListener { findNavController().navigate(MainFragmentDirections.actionMainFragmentToTopicsFragment()) }
+
+
+        //get the drawable
+        val myFabSrc = resources.getDrawable(R.drawable.ic_home)
+
+        //copy it in a new one
+        val willBeWhite = myFabSrc.constantState!!.newDrawable()
+
+        //set the color filter, you can use also Mode.SRC_ATOP
+        willBeWhite.mutate().setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
+
+        //set it to your fab button initialized before
+        binding.fab.setImageDrawable(willBeWhite)
+
+        binding.bottomNavigationView.menu.findItem(R.id.home).isChecked = true
 
     }
 }
